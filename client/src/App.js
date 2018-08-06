6bec9770b724a90e9d920f84e2586fb20149db3f
qiweiii <yangqiwei97@gmail.com>
2018-08-07 09:55:11 +1000

    'change padding'

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         <Layout.Header>
           <Navbar />
         </Layout.Header>
-        <Layout.Content style={{padding: 80, margin: 0, minHeight: 800}}>
+        <Layout.Content style={{padding: 40, margin: 0, minHeight: 800}}>
           <Row>
             <Col offset={4} span={16}>
               <Main />
