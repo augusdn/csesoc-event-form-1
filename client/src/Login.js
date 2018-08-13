b5cebe5e41349cc0200e7bcdfa59511cbd23afb8
qiweiii <yangqiwei97@gmail.com>
2018-08-14 02:27:28 +1000

    added login template

---
+++ client/src/Login.js
@@ -0,0 +1,60 @@
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import './styles/Login.css'
+
+// TODO:
+// How to make this a single page app with login?
+// Figure out how this code work.
+// Search: Single page app with login, react login tutorials, react login api, npm packages
+// maybe continue your udemy course...
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          // {getFieldDecorator('remember', {
+          //   valuePropName: 'checked',
+          //   initialValue: true,
+          // })(
+          //   <Checkbox>Remember me</Checkbox>
+          // )}
+          // <a className="login-form-forgot" href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          // Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+ReactDOM.render(<WrappedNormalLoginForm />, mountNode);
