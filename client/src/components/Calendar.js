b4463cf22d0531f2bca9b67046d6c3cc51fb9388
qiweiii <yangqiwei97@gmail.com>
2018-08-28 19:11:45 +1000

    fixed button, added iframe

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Calendar = () => (
   <div>
     <h1>Welcome to the Calendar!</h1>
+    <iframe src="https://calendar.google.com/calendar/embed?src=events.form%40csesoc.org.au&ctz=Australia%2FSydney" style={{border: "0", width: 800, height: 600, frameborder: "0", scrolling: "no"}}></iframe>
   </div>
 )
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,20 +4,25 @@ import { Link } from 'react-router-dom'
 import logo from '../static/csesocwhiteblue.png'
 import { AuthButton } from '../Auth.js'
 
+import { Affix } from 'antd';
+
 const Navbar = () => (
 
     <Menu
         theme="dark"
         mode="horizontal"
+        defaultSelectedKeys={['1']}
         style={{ lineHeight: '64px' , width: '100%'}}
     >
         <Menu.Item disabled={true}><img alt="CSESoc logo" src={logo}/></Menu.Item>
-        <Menu.Item><Link to='/'>Event-form</Link></Menu.Item>
-        <Menu.Item><Link to='/calendar'>Calendar</Link></Menu.Item>
-        <Menu.Item style><AuthButton/></Menu.Item>
-
+        <Menu.Item key="1"><Link to='/'>Event-form</Link></Menu.Item>
+        <Menu.Item key="2"><Link to='/calendar'>Calendar</Link></Menu.Item>
+        <Affix style={{ position: 'absolute', top: '1px', right: 20}}>
+         <AuthButton/>
+        </Affix>
     </Menu>
 
 )
 
+//<Menu.Item style={{justify: "end"}}><AuthButton/></Menu.Item>
 export default Navbar
